8557b40821fc433f28aee71341638528b584309f
sergiubaluta <sergiubaluta@956365ec-077c-11df-8158-c7cbc0dd6240>
2010-06-10 13:04:45 +0000

    Major code refactoring, some function might be broken for now... use previous revision.

---
+++ decoderbase.cpp
@@ -0,0 +1,22 @@
+#include "decoderbase.h"
+#include "decodercsv.h"
+#include "decoderhdlc.h"
+
+DecoderBase::DecoderBase(QObject *parent, Configuration* config) :
+    QObject(parent)
+{
+    this->config = config;
+    packetMaxLen = config->get("_setup_","packet_max_len","256").toInt();
+    packetStarted = false;
+}
+
+
+
+
+DecoderBase* createDecoder(QObject *parent, Configuration* config){
+
+    if(config->get("_setup_","decoder") == "hdlc")
+        return new DecoderHdlc(parent,config);
+    //default
+        return new DecoderCsv(parent,config);
+};
